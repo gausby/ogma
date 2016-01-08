33e21ebab281ce455a4e851ee8ee787868354f3a
Martin Gausby <martin@gausby.dk>
2016-01-08 17:39:05 +0100

    Added tests for subscribe, unsubscribe and publish

--- test/ogma_test.exs
+++ test/ogma_test.exs
@@ -1,7 +1,28 @@
 defmodule OgmaTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "subscribing to a topic" do
+    Ogma.subscribe("foo")
+    Ogma.publish("foo", "bar")
+
+    assert_receive "bar"
+  end
+
+  test "a process should only receive messages from subscribed topics" do
+    Ogma.subscribe("bar")
+
+    Ogma.publish("foo", :foo)
+    Ogma.publish("bar", :bar)
+
+    refute_receive :foo
+    assert_receive :bar
+  end
+
+  test "unsubscribing from a topic" do
+    Ogma.subscribe("foo")
+    Ogma.unsubscribe("foo")
+    Ogma.publish("foo", "bar")
+
+    refute_receive "bar"
   end
 end
